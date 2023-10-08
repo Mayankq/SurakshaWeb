7447e4e4de8b553f53b865bc7843f6d14050fa62
hari vishwanath <mailtohari134@gmail.com>
2023-10-08 16:42:39 +0530

    Feat : Convert to React

---
+++ SurakshaOne/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"; // Import a CSS file for styling
+
+function App() {
+  return (
+    <div className="full-page-iframe-container">
+      <iframe title="My iframe" src="index.html" className="full-page-iframe"></iframe>
+    </div>
+  );
+}
+
+export default App;
